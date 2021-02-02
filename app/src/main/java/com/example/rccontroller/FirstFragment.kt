ea1821ce50ed38abb602a8ae9f2bf9d9ced5e63d
Binbin Mu <dinobunmu@gmail.com>
2021-02-01 18:35:03 -0600

    can turn on Bluetooth

---
+++ app/src/main/java/com/example/rccontroller/FirstFragment.kt
@@ -0,0 +1,50 @@
+package com.example.rccontroller
+
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private val REQUEST_ENABLE_BT: Int = 1
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if(bluetoothAdapter == null)
+        {
+            Toast.makeText(activity, "Bluetooth Device Not Available", Toast.LENGTH_LONG).show()
+
+        }else{
+            if (!bluetoothAdapter?.isEnabled){
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }else{
+                Toast.makeText(activity, "Bluetooth is On", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+    }
+}
